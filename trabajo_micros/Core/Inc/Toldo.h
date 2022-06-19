a807bcef2d23678d4f6743967d2e39a70409edb1
jmcabornero <jmcabornero@gmail.com>
2022-06-19 17:44:42 +0200

    comentarios

--- trabajo_micros/Core/Inc/Toldo.h
+++ trabajo_micros/Core/Inc/Toldo.h
@@ -11,18 +11,18 @@
 #include "main.h"
 
 int estado_toldo=0; //0 parado, 1 subiendo, 2 bajando
-int flag_subida=0;	//flags que indican si el toldo está totalmente subido o bajado
+int flag_subida=0;	//flags que indican si el toldo estï¿½ totalmente subido o bajado
 int flag_bajada=0;
 
 void setEstadoToldo(int n){
 	estado_toldo=n;
-	if(estado_toldo==0){
+	if(estado_toldo==0){//parado
 		 HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3, GPIO_PIN_RESET);//3 subiendo
 		 HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_RESET);//4 bajando
-	}else if(estado_toldo==1){
+	}else if(estado_toldo==1){//subiendo
 		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3, GPIO_PIN_SET);
 		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_RESET);
-	}else if(estado_toldo==2){
+	}else if(estado_toldo==2){//bajando
 		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3, GPIO_PIN_RESET);
 		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_SET);
 	}
